13b6fec19b71b478c0d9c2982f793b534422f892
Ilhama Khudiyeva <ilhamexudiyeva5@gmail.com>
2022-03-22 14:24:20 +0400

    update

---
+++ HelloPython/app.py
@@ -0,0 +1,51 @@
+from binascii import a2b_base64
+
+
+# print("Hello World!")
+# a=input("1 -ci ededi daxil edin: ")
+# b=input("2-ci ededi daxil edin: ")
+# c=int(a)*int(b)
+# print("Ededlerin hasili: ",c)
+# d=int(input("1-ci eded: "))
+# e=int(input("2-ci eded: "))
+# f=d/e
+# print("Nisbet: ",f)
+
+# a=int(input("Ededi daxil edin: "))
+# if a%2==0:
+    # print("Cut")
+# else:
+    # print("Tek")
+
+# sual="Paytaxt haradir? "
+# cavab=input(sual)
+# if(cavab=="Baki") or (cavab=="baki"):
+    # print("Dogrudur!")
+# else:
+    # print("Yanlishdir!")
+    # print("Uduzdunuz!")
+
+# a=int(input("Ededi daxil edin: "))
+# if a>0:
+    # print("Musbet")
+# elif a<0:
+    # print("Menfi")
+# else:
+    # print("Sifir")
+
+# a=int(input("Ededi daxil edin: "))
+# if a>0:
+    # print("Musbet")
+# else:
+    # if a<0:
+        # print("Menfi")
+    # else:
+        # print("Sifir")
+
+a=int(input("Ededi daxil edin: "))
+if a%2==0:
+    print("Cut")
+if a%2!=0:
+    print("tek")
+
+
