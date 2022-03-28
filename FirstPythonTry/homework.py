62768870d76d082d51c18d2d4f2a0c9c41f33681
Ilhama Khudiyeva <ilhamexudiyeva5@gmail.com>
2022-03-28 07:34:37 +0400

    update

---
+++ FirstPythonTry/homework.py
@@ -0,0 +1,19 @@
+# Printing of numbers from 1 to 200
+# for i in range(1,201):
+#     print(i)
+# print(i)   
+
+# Printing the sum of numbers from 1 to 200
+# s=0
+# for i in range(1,201):
+#     s=s+i
+#     print(s)
+# print(s)
+
+# Printing the sum of single numbers from 1 to 500
+s=0
+for i in range(1,501):
+    if i%2!=0:
+        s=s+i
+        print(s)
+print(s)        
