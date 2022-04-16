3b47278e4e0cf654d497836725bdfc3fd021b01a
Ilhama Khudiyeva <ilhamexudiyeva5@gmail.com>
2022-04-16 14:30:11 +0400

    update

---
+++ PythonFunctions/task.py
@@ -0,0 +1,28 @@
+def showFilteredNumbers(a,b):
+    # a və b-nin ədəd olduğunu dəqiqləşdirin.a və ya b ədəd deyilsə "Düzgün dəyər daxil edilməyib" return edilsin
+    # a b-dən kiçikdirsə ekrana "Daxil edilən dəyərlər tələblərə uyğun deyil" çap edin
+    # a b-dən böyükdürsə a və b ədədləri arasında 5-ə bölünən amma 7-yə bölünməyən ədədləri ekrana çap edin
+    pass
+def sumOfNumbers(a,b):
+    # a və b-nin ədəd olduğunu dəqiqləşdirin.a və ya b ədəd deyilsə "Düzgün dəyər daxil edilməyib" return edilsin
+    # a və b arasında olan ədədlərin cəmini return edin
+    pass
+def findMiddleNumber(a,b):
+    # a və b-nin ədəd olduğunu dəqiqləşdirin.a və ya b ədəd deyilsə "Düzgün dəyər daxil edilməyib" return edilsin
+    # a və b ədədlərinin ədədi ortasından böyük olan ədədləri çap edin
+    pass
+def countRise():
+    # Şahmat taxtasını düşünün.1-ci xanaya bir ədəd buğda yerləşdirilir.Daha sonra növbəti xanaya bir öncəki xanadakı buğda sayının 2 qatı qədər buğda yerləşdirilir.Sizdən tələb olunan bütün şahmat taxtasında olan buğda sayını , bir buğdanın ağırlığının 0.04qr olduğunu qəbul edərək neçə ton buğda olduğunu hesablamağınızdır.
+    pass
+def ededinReqemSayiniTap(a):
+    # Daxil edilən ədədin rəqəm sayını ekrana çap edin
+    pass
+def ededinReqemleriniCapEt(a):
+    # Daxil edilən ədədin rəqəmlerini ayrı ayrı çap edin
+    pass
+def ededinReqemlerininCeminiTap(a):
+    # Daxil edilən ədədin rəqəmlerinin cemini tap
+    pass
+def ededinReqemlerininCeminiTap(a):
+    # Daxil edilən ədədin rəqəmlerinin cemini tap
+    pass
